bc80e4aa54e2c35c5b1a5a5e725f6023fcd684a8
Stanley Huang <infamous_godhand@yahoo.com>
2021-04-16 14:33:20 +1000

    getting movie data

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import axios from "axios";
 
 export default function App() {
+  const fetchMovieData = async () => {
+    const data = await axios.get("http://localhost:8000/movies");
+    console.log(data);
+  };
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
   return (
     <React.Fragment>
       <CssBaseline />
