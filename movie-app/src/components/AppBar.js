bb4547c2b6d308a782926130f2a2b227a69a8c3d
Stanley Huang <infamous_godhand@yahoo.com>
2021-04-17 13:38:27 +1000

    make home button navigate back

--- movie-app/src/components/AppBar.js
+++ movie-app/src/components/AppBar.js
@@ -4,7 +4,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import HomeIcon from "@material-ui/icons/Home";
+import { Link as RouterLink } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,8 +31,10 @@ export default function ButtonAppBar() {
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+            to="/"
+            component={RouterLink}
           >
-            <MenuIcon />
+            <HomeIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Movies Sessions
